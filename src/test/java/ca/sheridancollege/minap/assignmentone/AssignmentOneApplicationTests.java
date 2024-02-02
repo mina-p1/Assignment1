a1bf57a6ed7179cf7d508cc1f9b7c5c02d47ff6f
mina-p1 <minap@sheridanc.on.ca>
2024-02-01 23:17:11 -0500

    Started Project

---
+++ src/test/java/ca/sheridancollege/minap/assignmentone/AssignmentOneApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.minap.assignmentone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
