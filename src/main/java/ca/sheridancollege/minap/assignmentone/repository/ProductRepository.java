02320ad44de04372d5e1d5226b56f3440e992a89
mina-p1 <minap@sheridanc.on.ca>
2024-02-04 18:03:42 -0500

    Fixed css and fixed error in product page

--- src/main/java/ca/sheridancollege/minap/assignmentone/repository/ProductRepository.java
+++ src/main/java/ca/sheridancollege/minap/assignmentone/repository/ProductRepository.java
@@ -7,13 +7,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    // This interface will automatically have methods such as:
-    // - findAll()
-    // - findById()
-    // - save()
-    // - deleteById()
-    // ... and more, provided by JpaRepository.
 
-    // You can define custom queries as needed, for example:
-    // List<Product> findByNameContaining(String keyword);
 }
